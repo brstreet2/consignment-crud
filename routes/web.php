0e04be04f319ecfad0a6e5ad1cb678c75f4a8179
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-06-20 16:32:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,13 @@ Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'inde
 Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile');
 
 Route::get('/settings', [App\Http\Controllers\SettingsController::class, 'index'])->name('settings');
+
+Route::resource('banner', App\Http\Controllers\CMS\BannerController::class);
+
+// Route::group(['prefix' => 'banner'], function () {
+//     Route::get('', [App\Http\Controllers\CMS\BannerController::class, 'index'])->name('banner.index');
+//     Route::post('/create', [App\Http\Controllers\CMS\BannerController::class, 'create'])->name('banner.create');
+//     Route::get('/read/{id}', [App\Http\Controllers\CMS\BannerController::class, 'read'])->name('banner.read');
+//     Route::put('/update/{id}', [App\Http\Controllers\CMS\BannerController::class, 'update'])->name('banner.update');
+//     Route::delete('/delete/{id}', [App\Http\Controllers\CMS\BannerController::class, 'delete'])->name('banner.delete');
+// });
